4366b102dd379ef18a2202ad3a98915fd79137d6
Fabio Morety <you@example.com>
2025-01-31 17:09:32 -0300

    31 Enero 17.09 V4

--- Paginas/Bitacora.py
+++ Paginas/Bitacora.py
@@ -3,42 +3,71 @@ from tkinter import ttk
 from Tools.CrearObj import *
 
 from Tools.FuncionesPage2 import *
+from tkinter import Scrollbar
 
 def Form_Bitacora(parent,window):
 
     Matriz_CAPEX = Data_Bitacora()
 
     marco = tk.LabelFrame(parent,text="Planilla Bitácora",font=("Arial",10,"bold")); marco.pack(side="top",padx=12,pady=5 ,ipady=5, ipadx=5, fill="both", expand="yes")
+    canvas = tk.Canvas(marco)
+    scrollbar_y = tk.Scrollbar(marco, orient="vertical", command=canvas.yview)
+    scrollbar_x = tk.Scrollbar(marco, orient="horizontal", command=canvas.xview)
+    scrollable_frame = tk.Frame(canvas)
 
-    AgregarMovimiento = ttk.Button(marco,text="+",width=3)
+    scrollable_frame.bind(
+        "<Configure>",
+        lambda e: canvas.configure(
+            scrollregion=canvas.bbox("all")
+        )
+    )
+
+    canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
+    canvas.configure(yscrollcommand=scrollbar_y.set, xscrollcommand=scrollbar_x.set)
+    canvas.config(highlightthickness=0)
+
+    scrollbar_x.pack(side="bottom", fill="x")
+    canvas.pack(side="left", fill="both", expand=True)
+    scrollbar_y.pack(side="right", fill="y")
+
+    AgregarMovimiento = ttk.Button(scrollable_frame,text="+",width=3,command=lambda: Agregar_Movimiento(AgregarMovimiento,scrollable_frame,Matriz_CAPEX,linea))
     AgregarMovimiento.grid(row=1,column=0,sticky="e",padx=(8,0))
     
-    Label_ID_Activo = tk.Label(marco, text="ID Activo",font=("Arial",9,"bold")); Label_ID_Activo.grid(row=0,column=1,padx=5,pady=2)
-    ID_Activo = tk.Entry(marco,bd=1, highlightthickness=1, highlightbackground="gray",width=8,justify="center",font=("Open Sans",10)); ID_Activo.grid(row=1,column=1,padx=5)
-    ID_Activo.bind("<KeyPress>",lambda event: Entrega_Info(ID_Activo,marco,Matriz_CAPEX,event))
+    Label_ID_Activo = tk.Label(scrollable_frame, text="ID Activo",font=("Arial",9,"bold")); Label_ID_Activo.grid(row=0,column=1,pady=2)
+    ID_Activo = tk.Entry(scrollable_frame,bd=1, highlightthickness=1, highlightbackground="gray",width=4,justify="center",font=("Open Sans",10)); ID_Activo.grid(row=1,column=1)
+    ID_Activo.bind("<KeyPress>",lambda event: Entrega_Info(ID_Activo,scrollable_frame,Matriz_CAPEX,event))
 
-    Label_ID_Solicitud = tk.Label(marco, text="ID Solicitud",font=("Arial",9,"bold")); Label_ID_Solicitud.grid(row=0,column=2,padx=5,pady=2)
-    ID_Solicitud = tk.Label(marco, text="-",font=("Arial",9)); ID_Solicitud.grid(row=1,column=2,padx=5)
+    Label_ID_Solicitud = tk.Label(scrollable_frame, text="ID Solicitud",font=("Arial",9,"bold")); Label_ID_Solicitud.grid(row=0,column=2,pady=2,padx=25)
+    ID_Solicitud = tk.Label(scrollable_frame, text="-",font=("Arial",9)); ID_Solicitud.grid(row=1,column=2,padx=25)
 
-    Label_OCO = tk.Label(marco, text="OCO",font=("Arial",9,"bold")); Label_OCO.grid(row=0,column=3,padx=5,pady=2)
-    OCO = tk.Label(marco, text="-",font=("Arial",9)); OCO.grid(row=1,column=3,padx=5)
+    linea1 = agregar_linea(scrollable_frame,0,0,0,20); linea1.grid(row=1,column=3,sticky="ew")
 
-    Label_NomSol = tk.Label(marco, text="Nombre Solicitud",font=("Arial",9,"bold")); Label_NomSol.grid(row=0,column=4,padx=5,pady=2)
-    NomSol = tk.Label(marco, text="-",font=("Arial",9)); NomSol.grid(row=1,column=4,padx=5)
+    Label_OCO = tk.Label(scrollable_frame, text="OCO",font=("Arial",9,"bold")); Label_OCO.grid(row=0,column=4,pady=2,padx=25)
+    OCO = tk.Label(scrollable_frame, text="-",font=("Arial",9)); OCO.grid(row=1,column=4,padx=25)
 
-    Label_Item = tk.Label(marco, text="Ítem",font=("Arial",9,"bold")); Label_Item.grid(row=0,column=5,padx=5,pady=2)
-    Item = tk.Label(marco, text="-",font=("Arial",9)); Item.grid(row=1,column=5,padx=5)
+    linea2 = agregar_linea(scrollable_frame,0,0,0,20); linea2.grid(row=1,column=5,sticky="ew")
 
-    Label_Monto_PostRe = tk.Label(marco, text="Post Resolución",font=("Arial",9,"bold")); Label_Monto_PostRe.grid(row=0,column=6,padx=(5,10),pady=2)
-    Monto_PostRe = tk.Label(marco, text="-",font=("Arial",9)); Monto_PostRe.grid(row=1,column=6,padx=(5,10))
+    Label_NomSol = tk.Label(scrollable_frame, text="Nombre Solicitud",font=("Arial",9,"bold")); Label_NomSol.grid(row=0,column=6,pady=2,padx=70)
+    NomSol = tk.Label(scrollable_frame, text="-",font=("Arial",9)); NomSol.grid(row=1,column=6,padx=70)
 
-    linea = agregar_linea(marco,0,10,0,80); linea.grid(row=0,column=7,rowspan=2,sticky="ew") 
+    linea3 = agregar_linea(scrollable_frame,0,0,0,20); linea3.grid(row=1,column=7,sticky="ew")
 
-    Label_Movimiento = tk.Label(marco, text="Monto",font=("Arial",9,"bold")); Label_Movimiento.grid(row=0,column=8,padx=(10,5),pady=2)
-    Movimiento = tk.Entry(marco, bd=1, highlightthickness=1, highlightbackground="gray",font=("Open Sans",10),width=14); Movimiento.grid(row=1,column=8,padx=(10,5))
+    Label_Item = tk.Label(scrollable_frame, text="Ítem",font=("Arial",9,"bold")); Label_Item.grid(row=0,column=8,pady=2,padx=70)
+    Item = tk.Label(scrollable_frame, text="-",font=("Arial",9)); Item.grid(row=1,column=8,padx=70)
 
-    Label_Motivo = tk.Label(marco, text="Motivo",font=("Arial",9,"bold")); Label_Motivo.grid(row=0,column=9,padx=5,pady=2)
-    Motivo = ttk.Combobox(marco, values=["Ahorro","Suplemento","Postergación","Cierre"], state="readonly"); Motivo.grid(row=1,column=9,padx=5)
+    linea4 = agregar_linea(scrollable_frame,0,0,0,20); linea4.grid(row=1,column=9,sticky="ew")
 
-    Label_Ticket = tk.Label(marco, text="Ticket/Correo",font=("Arial",9,"bold")); Label_Ticket.grid(row=0,column=10,padx=5,pady=2)
-    Ticket = tk.Entry(marco, bd=1, highlightthickness=1, highlightbackground="gray"); Ticket.grid(row=1,column=10,padx=5)
+    Label_Monto_PostRe = tk.Label(scrollable_frame, text="Post Resolución",font=("Arial",9,"bold")); Label_Monto_PostRe.grid(row=0,column=10,padx=(4,10),pady=2)
+    Monto_PostRe = tk.Label(scrollable_frame, text="-",font=("Arial",9)); Monto_PostRe.grid(row=1,column=10,padx=(4,10))
+
+    linea = agregar_linea(scrollable_frame,0,5,0,80); linea.grid(row=0,column=11,rowspan=2,sticky="ew") 
+
+    Label_Movimiento = tk.Label(scrollable_frame, text="Monto",font=("Arial",9,"bold")); Label_Movimiento.grid(row=0,column=12,padx=(10,5),pady=2)
+    Movimiento = tk.Entry(scrollable_frame, bd=1, highlightthickness=1, highlightbackground="gray",font=("Open Sans",10),width=14); Movimiento.grid(row=1,column=12,padx=(10,5))
+
+    Label_Motivo = tk.Label(scrollable_frame, text="Motivo",font=("Arial",9,"bold")); Label_Motivo.grid(row=0,column=13,pady=2,padx=5)
+    Motivo = ttk.Combobox(scrollable_frame, values=["Ahorro","Suplemento","Postergación","Cierre"], state="readonly"); Motivo.grid(row=1,column=13,padx=5)
+
+    Label_Ticket = tk.Label(scrollable_frame, text="Ticket/Correo",font=("Arial",9,"bold")); Label_Ticket.grid(row=0,column=14,pady=2,padx=5)
+    Ticket = tk.Entry(scrollable_frame, bd=1, highlightthickness=1, highlightbackground="gray"); Ticket.grid(row=1,column=14,padx=5)
+
--- Tools/FuncionesPage2.py
+++ Tools/FuncionesPage2.py
@@ -1,3 +1,7 @@
+import tkinter as tk
+from tkinter import ttk
+from Tools.CrearObj import agregar_linea
+
 import re
 import requests
 from SQLConnect import SQLConsulta as SQL
@@ -20,6 +24,11 @@ def Entrega_Info(ID,parent,matriz,event):
 
     #Primero se restringe el ingreso de caracteres no numéricos y se limita la cantidad de caracteres
     if event.keysym in ("BackSpace", "Delete"):
+        if ID.get()[:-1] == "":
+            for widget in parent.grid_slaves():
+                fila_actual = ID.grid_info()["row"]
+                if widget.winfo_class() == "Label" and widget.grid_info()["row"] == fila_actual:
+                    widget.config(text="-")
         return    
     elif not event.char.isdigit():
         return "break"
@@ -34,21 +43,26 @@ def Entrega_Info(ID,parent,matriz,event):
         return "break"
 
     info = matriz.loc[matriz['ID_Activo'] == int(ID_Activo)]
-    columna = 0
+    columna = 0; n=0
 
     print(info) if not info.empty else print("No se encontró información")
 
     if not info.empty:
         info_dict = info.to_dict('records')[0]
         for key, value in info_dict.items():
-            columna += 1
+            columna += 1+1*n
             
+            #Se formatean los datos para que se vean mejor en la interfaz
             if key == 'ID_Activo':
                 continue
             elif key == 'OCO':
                 value = int(value)
             elif key == 'Post_Resolucion':
                 value = "${:,.0f}".format(int(value)).replace(',', '.')
+            elif key == 'Nombre_Solicitud' or key == 'Item':
+                value = value[:50]+"..." if len(value) > 50 else value
+
+            n=1
 
             for widget in parent.grid_slaves():
                 
@@ -56,12 +70,56 @@ def Entrega_Info(ID,parent,matriz,event):
                 
                 if widget.winfo_class() == "Label" and widget.grid_info()["row"] == fila_actual and widget.grid_info()["column"] == columna:
                     widget.config(text=value)
+                    if key == "Nombre_Solicitud" or key == "Item":
+                        widget.grid(padx=70-len(value))
+                    elif key == "OCO" or key == "ID_Solicitud":
+                        widget.grid(padx=25-len(str(value)))
+                    
+                        
+                    
                     break
     else:
         for widget in parent.grid_slaves():
                 fila_actual = ID.grid_info()["row"]
                 if widget.winfo_class() == "Label" and widget.grid_info()["row"] == fila_actual:
                     widget.config(text="-")
+
+def Agregar_Movimiento(boton,parent,matriz,linea):
+    #Se crea una nueva fila en la Bitácora
+    # Se crea un nuevo botón para agregar un nuevo movimiento luego
+        
+    fila_nueva = boton.grid_info()["row"]+1
+
+    boton.grid(row=fila_nueva)
+    
+    ID_Activo = tk.Entry(parent,bd=1, highlightthickness=1, highlightbackground="gray",width=4,justify="center",font=("Open Sans",10)); ID_Activo.grid(row=fila_nueva,column=1)
+    ID_Activo.bind("<KeyPress>",lambda event: Entrega_Info(ID_Activo,parent,matriz,event))
+
+    ID_Solicitud = tk.Label(parent, text="-",font=("Arial",9)); ID_Solicitud.grid(row=fila_nueva,column=2,padx=25)
+
+    linea1 = agregar_linea(parent,0,0,0,20); linea1.grid(row=fila_nueva,column=3,sticky="ew")
+
+    OCO = tk.Label(parent, text="-",font=("Arial",9)); OCO.grid(row=fila_nueva,column=4,padx=25)
+
+    linea2 = agregar_linea(parent,0,0,0,20); linea2.grid(row=fila_nueva,column=5,sticky="ew")
+
+    NomSol = tk.Label(parent, text="-",font=("Arial",9)); NomSol.grid(row=fila_nueva,column=6,padx=70)
+
+    linea3 = agregar_linea(parent,0,0,0,20); linea3.grid(row=fila_nueva,column=7,sticky="ew")
+
+    Item = tk.Label(parent, text="-",font=("Arial",9)); Item.grid(row=fila_nueva,column=8,padx=70)
+
+    linea4 = agregar_linea(parent,0,0,0,20); linea4.grid(row=fila_nueva,column=9,sticky="ew")
+
+    Monto_PostRe = tk.Label(parent, text="-",font=("Arial",9)); Monto_PostRe.grid(row=fila_nueva,column=10,padx=(4,10))
+
+    linea = agregar_linea(parent,0,5,0,80); linea.grid(row=0,column=11,rowspan=2,sticky="ew") 
+
+    Movimiento = tk.Entry(parent, bd=1, highlightthickness=1, highlightbackground="gray",font=("Open Sans",10),width=14); Movimiento.grid(row=fila_nueva,column=12,padx=(10,5))
+
+    Motivo = ttk.Combobox(parent, values=["Ahorro","Suplemento","Postergación","Cierre"], state="readonly"); Motivo.grid(row=fila_nueva,column=13,padx=5)
+
+    Ticket = tk.Entry(parent, bd=1, highlightthickness=1, highlightbackground="gray"); Ticket.grid(row=fila_nueva,column=14,padx=5)
 
 
     
